77ab2199a94b7934e79a55a191f8709d11582703
Maiara Ávila <maiaroviski@gmail.com>
2025-10-27 17:49:32 -0300

    Tipos de variáveis

---
+++ index4.js
@@ -0,0 +1,15 @@
+function testandoVariaveis(){
+   const texto = "João" //string
+   const numero = 1 //number 
+   const boolean = true //true or false 
+   const array = ["1", 1, true,{}]; //vários tipos de variáveis juntas
+   const objeto ={nome:"Gol", ano: 1987, km: "18.000 Km", primeiroDono: false}
+
+   console.log(array[0]);
+   console.log(objeto.ano)
+
+    
+    
+}
+
+testandoVariaveis()
