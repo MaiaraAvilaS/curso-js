4193173b4b574cb78953730f124ff0e9e2cc148d
Maiara Ávila <maiaroviski@gmail.com>
2025-10-27 18:06:20 -0300

    Aprendendo If

---
+++ index7.js
@@ -0,0 +1,14 @@
+function condicional(){
+
+    const numero = 7    
+
+    if (numero + 3 === 10){
+        console.log("Soma certa!");
+    }else{
+        console.log("Soma errada!");
+    }
+
+   
+}
+
+condicional();
