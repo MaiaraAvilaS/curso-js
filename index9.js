ba07e5a92ffe050d3823c7d6e924ce0dbf2f8c5e
Maiara Ávila <maiaroviski@gmail.com>
2025-10-27 18:15:23 -0300

    Switch Case

---
+++ index9.js
@@ -0,0 +1,19 @@
+function condicional(){
+    const fruta = "Abacaxi";
+
+    switch(fruta){
+        case "Banana":
+            console.log("É banana");
+            break;
+        case "Uva":
+            console.log("É uva");
+            break;
+        case "Maça":
+            console.log("É maçã");
+            break;    
+        default:
+            console.log("Nenhuma das frutas")
+    }
+}
+
+condicional()
