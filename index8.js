89e02735a103cd74c25908b82c9d7da018f99e8f
Maiara Ávila <maiaroviski@gmail.com>
2025-10-27 18:09:53 -0300

    Else If

---
+++ index8.js
@@ -0,0 +1,16 @@
+function condicional(){
+
+    const numero = 6  
+
+    if (numero + 3 === 10){
+        console.log("Soma certa!");
+    }else if(numero + 4 === 10){
+        console.log("Segunda soma certa!");
+    }else{
+        console.log("Soma errada!");
+    }
+
+   
+}
+
+condicional();
