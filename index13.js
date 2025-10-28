f5ce2d2a8089c85c5ccb9314786a5c51b59f2115
Maiara Ávila <maiaroviski@gmail.com>
2025-10-28 17:19:56 -0300

    Concatenar Arrays e Acessar Item

--- index13.js
+++ index13.js
@@ -3,11 +3,13 @@ function concatenar (){
 
     var nomesAdicionar = ["Carlos", "Abel", "Isaac"];
 
-    var nomesConcatenar = nomes.concat(nomesAdicionar)
+    //var nomesConcatenar = nomes.concat(nomesAdicionar)
+    var nomesSpread = [...nomes, ...nomesAdicionar]
 
     console.log(nomes);
     console.log(nomesAdicionar);
-    console.log(nomesConcatenar)
+    //console.log(nomesConcatenar);
+    console.log(nomesSpread[5])
 
 }
 
